14afdd898d7e2a64c92bc197540cd5a6c64e78b1
jarnomikkola1 <62621324+jarnomikkola1@users.noreply.github.com>
2024-12-27 04:16:47 +0200

    Add files via upload

---
+++ JarnosGooGunConfig.cs
@@ -0,0 +1,34 @@
+﻿using System; 
+using PhoenixPoint.Modding;
+
+namespace JarnosGooGun
+{
+	/// <summary>
+	/// ModConfig is mod settings that players can change from within the game.
+	/// Config is only editable from players in main menu.
+	/// Only one config can exist per mod assembly.
+	/// Config is serialized on disk as json.
+	/// </summary>
+	public class JarnosGooGunConfig : ModConfig
+	{
+
+		/*
+        /// Only public fields are serialized.
+        /// Supported types for in-game UI are:
+        public int IntegerValue;
+        public float FloatValue;
+        public bool BoolValue;
+
+        public enum CustomEnum
+        {
+            A, B, C
+        }
+        public CustomEnum CustomEnumValue;
+        */
+
+		[ConfigField(text: "Edit Kaos Buggy/Junker to have a paralyzing goo gun",
+			description: "Just like the titel say (Default false)")]
+		public bool maxgoo = false;
+
+	}
+}
